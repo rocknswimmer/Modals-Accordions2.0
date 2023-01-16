c1ae461cddbd8344935a0b17494d5447c9be585f
Andy Simmons <rocknswimmer116@gmail.com>
2023-01-16 13:01:19 -0500

    imported work and made image work in presentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,80 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Modal from './modal';
+import Accordion from './accordion';
+import {prezyData} from './content';
+import './App.css';
 
 function App() {
+  const [startPrezy, setStartPrezy] = useState(false);
+  const [endPrezy, setEndPrezy] = useState(true);
+  const [displayAcc, setDisplayAcc] = useState(false);
+  const [startInnerPrezy, setStartInnerPrezy] = useState(false);
+  const [secondSlide, setSecondSlide] = useState(false);
+  const [showData, setShowData] = useState(false);
 
+  const openModal = () => {
+    setStartPrezy(!startPrezy);
+    setEndPrezy(!endPrezy);
+  }
+  const openAccordian = () => {
+    setStartPrezy(!startPrezy);
+    setDisplayAcc(!displayAcc);
+  }
+
+  //This is going to be really ugly and bad practice for speed and easy since im using modals and accordians to do a silde show prezy within  a prezy cleaner loop without prezy commented out below
   return (
-    <div>
-      <h1>Site Title</h1>
+    <div className="App">
+      {endPrezy && <button onClick={openModal}>OPEN MODAL EXAMPLE</button>}
+      {startPrezy &&
+      <Modal
+        close={() => {setStartPrezy(!startPrezy);}}
+        content={<div><div>Modal/Dialog - These are for getting work done. (Example: a button that says "Invite Friends" will open such a container with a list friends you can then invite). These let you do more work without having to show all the information on the main screen. These messages are not locked and you can click anywhere to dismiss the container.</div><br/><div>Popup - You don't need to deal with these messages right away, yet at some point you will need to take action since these won't go away until explicitly say say you don't want them around anymore.</div> <br/><button onClick={openAccordian}>OPEN ACCORDION EXAMPLE</button></div>}
+      />}
+      {/* will propable need to make an accodion feed */}
+      {displayAcc &&
+      <Accordion
+        title={'ACCORDION EXAMPLE'}
+        content={
+        <div>
+          The term stems from the musical accordion in which sections of the bellows can be expanded by pulling outward. A common example of an accordion is the Show/Hide operation of a box region, but extended to have multiple sections in a list.
+        <Accordion
+        title={'???'}
+        content={ <button onClick={() => { setDisplayAcc(!displayAcc); setStartInnerPrezy(!startInnerPrezy); }}>???</button>}
+       />
+        </div>}
+       />}
+       {startInnerPrezy &&
+       <Modal
+         close={() => { setStartInnerPrezy(!startInnerPrezy); }}
+         content={
+           <div>
+            {prezyData.modalData.firstModal.content} <br/>
+            <button onClick={() => { setStartInnerPrezy(!startInnerPrezy); setSecondSlide(!secondSlide); }}>Next Slide</button>
+           </div>
+        }
+       />}
+       {secondSlide &&
+       <Modal
+         close={() => { setSecondSlide(!secondSlide); }}
+         content={
+           <div>
+            {prezyData.modalData.secondModal.content} <br/>
+            <button onClick={() => { setShowData(!showData); setSecondSlide(!secondSlide); }}>To Accordion For Data</button>
+           </div>
+        }
+       />}
+       {showData &&
+       <div>
+        <h1>Laughter Data</h1>
+        <div>
+          {prezyData.accordionData.map(({title, content}, i) => {
+            return <Accordion title={title} content={content} key={i} />
+          })}
+        </div>
+        <button onClick={() => { setEndPrezy(!endPrezy); setShowData(!showData); } }>END PREZY</button>
+       </div>
+       }
     </div>
-  )
+  );
 }
-
 export default App;
---
+++ src/content.js
@@ -0,0 +1,29 @@
+import React from 'react';
+export const prezyData = {
+  modalData: {
+  firstModal: {
+    content: (<div><h1>Laughter</h1><h3>by: Andy Simmons</h3></div>)
+  },
+  secondModal: {
+    content: (<img src="/laghing emoji.jpeg" alt="laughing emoji" />)
+    // durring the presentation I used the example that I messed something up with create react app and so imgages were not working and I had to laugh it off and just use a react icon to save time
+  }
+},
+accordionData: [
+  {
+    title: 'Info Section 1',
+    content: (<div><h1>Short-term benefits</h1> <div>Activate and relieve your stress response. A rollicking laugh fires up and then cools down your stress response, and it can increase and then decrease your heart rate and blood pressure. The result? A good, relaxed feeling.</div><br/><div>Soothe tension. Laughter can also stimulate circulation and aid muscle relaxation, both of which can help reduce some of the physical symptoms of stress.</div></div>)
+  },
+  {
+    title: 'Info Section 2',
+    content: (<div><h1>Long-term benefits</h1> <div>Increase personal satisfaction. Laughter can also make it easier to cope with difficult situations. It also helps you connect with other people.</div><br/><div>Improve your mood. Many people experience depression, sometimes due to chronic illnesses. Laughter can help lessen your stress, depression and anxiety and may make you feel happier. It can also improve your self-esteem.</div></div>)
+  },
+  {
+    title: 'sources',
+    content: (<div><a href='https://www.mayoclinic.org/healthy-lifestyle/'>https://www.mayoclinic.org/healthy-lifestyle/stress-management/in-depth/stress-relief/art-20044456</a><br/><a href='https://www.laughteryoga.org/'>https://www.laughteryoga.org/</a><br/> modal vs popup: <a href='https://www.laughteryoga.org/'>https://ux.stackexchange.com/questions/90336/whats-the-difference-between-a-modal-popup-popover-and-lightbox</a></div>)
+  }
+
+
+]
+
+};
